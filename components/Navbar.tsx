47ce16b751bfbe436be025896f70325f27578929
Santy Samaniego <ssamaniego065@gmail.com>
2026-01-30 01:24:04 -0300

    fix logo path

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -49,7 +49,8 @@ const Navbar: React.FC = () => {
             {/* LOGO: Aquí puedes cambiar el tamaño en w-10 h-10 (móvil) y md:w-12 md:h-12 (desktop) */}
             <div className="relative w-10 h-10 md:w-12 md:h-12 flex-shrink-0">
               <img
-                src="../img/logo_ms.png"
+                src="/logo_ms.png"
+                alt="Logo Dental MS"
                 className="w-full h-full rounded-full bg-white object-contain p-1 border border-slate-100 shadow-sm"
               />
             </div>
